4723e334f407b86bf76cb61cf69f1b46fad27cc4
jan <jan.devisscher@student.uhasselt.be>
2022-12-09 12:02:42 +0100

    added route if page does not exist

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404 " + req.url + " not found</h1>")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
